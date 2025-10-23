a6db051d1cbf7185e3e9f7a5f719bc330e1b5311
SamOne777 <samone777@ejemplo.com>
2025-10-23 16:11:23 -0500

    Ejercicio propuesto 2

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,8 +1,8 @@
 print("Ejercicio propuesto 2")
 
 cantidad_litros = float(input("Ingresar litros de leche: "))
-cantidad_galones = cantidad_litros / 3.785
-precio = int(input("Ingresar el precio de galon de leche: "))
+cantidad_galones = cantidad_litros / 3.785                     #1 galon = 3.785 litros
+precio = float(input("Ingresar el precio de galon de leche: "))
 total_pago = cantidad_galones * precio
 print(f"TOTAL GALONES: {cantidad_galones}")
 print(f"TOTAL A PAGAR: {total_pago}")
